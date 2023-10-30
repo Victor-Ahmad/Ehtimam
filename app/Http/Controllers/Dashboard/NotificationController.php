9d4a37bdb4d4492c4fecd8618f1cd9a73a8af258
vicSahmtech <m.ahmad@sahmtech.com>
2023-10-30 17:08:18 +0300

    fixed notification on dashboard

--- app/Http/Controllers/Dashboard/NotificationController.php
+++ app/Http/Controllers/Dashboard/NotificationController.php
@@ -19,13 +19,31 @@ class NotificationController extends Controller
     use NotificationTrait;
     public function showNotification()
     {
+        $type = request()->input('type');
+        $gender = request()->input('gender');
+        $subjects = User::whereNotNull('fcm_token');
 
-        $technicians = Technician::query()->whereNotNull('fcm_token')->pluck('name', 'id')->all();
-        $customers = User::whereNotNull('fcm_token')->get();
-        $customersWithNoOrders = User::whereNotNull('fcm_token')->doesntHave('orders')->get();
+        if (request()->ajax()) {
+            if ($type == 'technician') {
+                $subjects = Technician::whereNotNull('fcm_token');
+            } elseif ($type == 'customersWithNoOrders') {
+                $subjects = User::whereNotNull('fcm_token')->doesntHave('orders');
+            }
+
+            if ($gender == 'male') {
+                $subjects->where('gender', 'male');
+            } elseif ($gender == 'female') {
+                $subjects->where('gender', 'female');
+            }
+            $subjects = $subjects->get();
+
+            return response()->json(['subjects' => $subjects]);
+        }
 
 
-        return view('dashboard.notification.notification', compact('customers', 'technicians', 'customersWithNoOrders'));
+
+        $subjects = $subjects->get();
+        return view('dashboard.notification.notification', compact('subjects'));
     }
 
     public function sendNotification(Request $request)
@@ -33,14 +51,16 @@ class NotificationController extends Controller
 
         $request->validate([
             'customer_id' => 'nullable',
-            'technician_id' => 'nullable',
+            'subject_id' => 'nullable',
             'title' => 'required',
             'message' => 'required',
-            'type' => 'required'
+            'type' => 'required',
+            'gender' => 'require'
         ]);
+
         if ($request->type == 'customer') {
             if ($request->customer_id == 'all') {
-                $FcmTokenArray = User::whereNotNull('fcm_token')->get()->pluck('fcm_token');
+                $FcmTokenArray = User::whereNotNull('fcm_token');
             } else {
                 $user = User::where('id', $request->customer_id)->first('fcm_token');
                 $FcmToken = $user->fcm_token;
@@ -56,26 +76,27 @@ class NotificationController extends Controller
             }
             $type = 'customer';
         } else {
-            if ($request->technician_id == 'all') {
-                $allTechn = Technician::whereNotNull('fcm_token')->get();
+            if ($request->subject_id == 'all') {
+                $FcmTokenArray = Technician::whereNotNull('fcm_token')();
 
-                $FcmTokenArray = $allTechn->pluck('fcm_token');
+                // $FcmTokenArray = $allTechn->pluck('fcm_token');
 
 
-                foreach ($allTechn as $tech) {
-                    Notification::send(
-                        $tech,
-                        new SendPushNotification($request->title, $request->message)
-                    );
-                }
+                // foreach ($allTechn as $tech) {
+                //     Notification::send(
+                //         $tech,
+                //         new SendPushNotification($request->title, $request->message)
+                //     );
+                // }
             } else {
-                $technician = Technician::where('id', $request->technician_id)->first();
+                $technician = Technician::where('id', $request->subject_id)->first();
+
                 $FcmToken = $technician->fcm_token;
 
-                Notification::send(
-                    $technician,
-                    new SendPushNotification($request->title, $request->message)
-                );
+                // Notification::send(
+                //     $technician,
+                //     new SendPushNotification($request->title, $request->message)
+                // );
             }
 
             $type = 'technician';
@@ -89,10 +110,13 @@ class NotificationController extends Controller
             return redirect()->back()->withErrors(['fcm_token' => 'لا يمكن ارسال الاشعارت لعدم توفر رمز الجهاز']);
         }
 
-        $message = str_replace('&nbsp;', ' ', strip_tags($request->message));
+        if (isset($FcmTokenArray) && count($FcmTokenArray) != 0 && $request->gender && $request->gender != 'all') {
+            $FcmTokenArray->where('gender', $request->gender);
+        }
 
+        $message = str_replace('&nbsp;', ' ', strip_tags($request->message));
         $notification = [
-            'device_token' => isset($FcmToken) ? [$FcmToken] : $FcmTokenArray,
+            'device_token' => isset($FcmToken) ? [$FcmToken] : $FcmTokenArray->pluck('fcm_token'),
             'title' => $request->title,
             'message' =>  $message,
             'type' => $type ?? '',
